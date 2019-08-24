bd8ff712d833c021b53f532049fe7818700ca5be
Brad Alexander <brad.alexander913@gmail.com>
2019-08-24 20:04:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+def sort_array_asc(array)
+  array.sort 
+end 
 
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort do |a|
+    if a.index == 1 
+      b<=>a 
+    end
+  end 
+end
